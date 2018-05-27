a2372abc236fd0d59825eb63c2782fa2007984f4
mrasmussen09 <36437425+mrasmussen09@users.noreply.github.com>
2018-05-28 00:50:35 +0200

    Add files via upload

--- Renset script.R
+++ Renset script.R
@@ -14,8 +14,8 @@
 #### 3) Inspicerer data                             ####
 #### 4) Kombinerer variable til brug for analyse    ####
 #### 5) Estimation af SEM model                     ####
-#### 6) Illustration af data                        ####
-#### 7) Afrapportering af estimation                ####
+#### 6) Test af fit                                 ####
+#### 7) Illustration                                ####
 ########################################################
 ########################################################
 ########################################################
@@ -35,7 +35,7 @@ library(lavaan) #Udviklet pakke til SEM estimation til forskningsbrug
 # Jeg begynder med at lave to datasæt; et for hver af de to datakilder jeg anvender
 
 # Laver datasæt med World Bank World Development Index 
-data_wdi <- WDI(country =c("AFG",	"AGO",	"AZE",	"BEN",	"BTN",	"BOL",	"BFA",	"BDI",	"KHM",	"CMR",	"CAF",	"TCD",	"COM",	"ZAR",	"COK",	"CIV",	"DJI", "GNQ",	"ERI",	"ETH",	"GMB",	"GHA",	"GIN",	"GNB",	"GUY",	"HTI",	"KAZ",	"KEN",	"PRK",	"KGZ",	"LAO",	"LSO",	"LBR",	"MDG",	"MWI",	"MLI",	"MRT",	"MDA",	"MNG",	"MOZ",	"MMR",	"NPL",	"NER",	"NGA",	"RWA",	"SEN",	"SLE",	"SOM",	"SDN",	"TJK",	"TZA",	"TGO",	"TKM",	"UGA",	"UZB",	"YEM",	"ZMB",	"ZWE"), indicator = c("VC.PKP.TOTL.UN", "IC.EXP.CSBC.CD","IC.IMP.CSBC.CD", "DT.ODA.OATL.CD", "NY.ADJ.DMIN.GN.ZS", "NY.GDP.PETR.RT.ZS"), start = 1974, end = 2017, extra = FALSE, cache = NULL)
+data_wdi <- WDI(country =c("AFG",	"AGO",	"AZE",	"BEN",	"BTN",	"BOL",	"BFA",	"BDI",	"KHM",	"CMR",	"CAF",	"TCD",	"COM",	"ZAR",	"COK",	"CIV",	"DJI", "GNQ",	"ERI",	"ETH",	"GMB",	"GHA",	"GIN",	"GNB",	"GUY",	"HTI",	"KAZ",	"KEN",	"PRK",	"KGZ",	"LAO",	"LSO",	"LBR",	"MDG",	"MWI",	"MLI",	"MRT",	"MDA",	"MNG",	"MOZ",	"MMR",	"NPL",	"NER",	"NGA",	"RWA",	"SEN",	"SLE",	"SOM",	"SDN",	"TJK",	"TZA",	"TGO",	"TKM",	"UGA",	"UZB",	"YEM",	"ZMB",	"ZWE"), indicator = c("VC.PKP.TOTL.UN", "IC.EXP.CSBC.CD","IC.IMP.CSBC.CD", "DT.ODA.OATL.CD", "NY.ADJ.DMIN.GN.ZS", "NY.GDP.PETR.RT.ZS"), start = 1975, end = 2015, extra = FALSE, cache = NULL)
 View(data_wdi)
 
 # Laver datasæt med Polity IV
@@ -51,14 +51,52 @@ polity <- "http://www.systemicpeace.org/inscr/p4v2016.xls"
 GET(polity, write_disk(polity.temp <- tempfile(fileext = ".xls")))
 # Read and clean Polity data
 data_pol4 <- read_excel(polity.temp) %>%
-  select(cowcode = scode, year, polity = polity2) %>%
-  mutate_at(vars(year, cowcode, polity),
+  select(scode, year, polity = polity2) %>%
+  mutate_at(vars(year, polity),
             funs(as.integer))%>%
-  filter(year == 1965:2015)
+  filter(year == 1975:2015)
 
 glimpse(data_pol4)
 View(data_pol4)
 
-# Begge datasæt klar til at blive kombineret. Samlet datasæt hedder data_bb (Bottom Billion)
-data_bb <- left_join()
+# Generere variable med gennemsnit til brug for bølger. Jeg vælger at anvende fire bølger med gennemsnit for 10 år.
+edt_wdi <- data_wdi %>%
+  mutate(udv1 = mean(year == 1975:1984)) %>%
+  mutate(udv2 = mean(year == 1985:1994)) %>%
+  mutate(udv2 = mean(year == 1995:1994)) %>%
+  mutate(udv2 = mean(year == 2005:2014))
+
+View(edt_wdi)
+
+edt_pol4 <- data_pol4 %>%
+  mutate(demo1 = mean(year == 1975:1984)) %>%
+  mutate(demo2 = mean(year == 1985:1994)) %>%
+  mutate(demo3 = mean(year == 1995:1994)) %>%
+  mutate(demo4 = mean(year == 2005:2014))
+        
+View(edt_pol4)
 ########################################################
+# 3) Inspicerer data og variable
+
+# 4) kombinerer datasæt
+# Begge datasæt klar til at blive kombineret. Samlet datasæt hedder data_bb (Bottom Billion)
+data_bb <- full_join(edt_wdi, edt_pol4)
+View(data_bb)
+# 5) Estimation af model
+model <- '
+# regressions
+demo1 ~ udv1
+demo2 ~ udv1 + udv2
+demo3 ~ udv2 + udv3
+demo4 ~ udv3 + udv4
+
+# residual correlations
+y1 ~~ y5
+y2 ~~ y4 + y6
+y3 ~~ y7
+y4 ~~ y8
+y6 ~~ y8
+
+6) test af fit
+fit <- sem(model, data=data_bb)
+summary(fit, standardized=FALSE)
